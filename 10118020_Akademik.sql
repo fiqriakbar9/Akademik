c88b269c7b7b62796f7b9cf692d4e0a5c130b127
fiqriakbar9 <42399322+fiqriakbar9@users.noreply.github.com>
2020-08-19 10:54:32 +0700

    Update 10118020_Akademik.sql

--- 10118020_Akademik.sql
+++ 10118020_Akademik.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
--- Host: localhost    Database: akademik
+-- Host: localhost    Database: 10118020_akademik
 -- ------------------------------------------------------
 -- Server version	8.0.18
 
